cf6b700d45033283827a88a7dbec95356c7c26f6
efekancl <efecelik4207@gmail.com>
2026-07-30 23:56:17 +0300

    port bilgisi PortHolder sınıfı üzerinden dinamik çekilecek şekilde düzenlendi

--- client/src/main/java/com/efekan/client/CustomInterceptor.java
+++ client/src/main/java/com/efekan/client/CustomInterceptor.java
@@ -1,6 +1,5 @@
 package com.efekan.client;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
@@ -13,17 +12,18 @@ import java.io.IOException;
 public class CustomInterceptor implements ClientHttpRequestInterceptor {
 
     private static final Logger log = LoggerFactory.getLogger(CustomInterceptor.class);
-    private final String serverPort;
-    public CustomInterceptor(String serverPort) {
-        this.serverPort = serverPort;
-    }
 
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte [] body, ClientHttpRequestExecution execution) throws IOException {
-        request.getHeaders().add("X-Client-Port", serverPort);
-        log.info("URL {}", request.getURI());
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        String serverPort = PortHolder.getServerPort();
+
+        if (serverPort != null && !serverPort.trim().isEmpty()) {
+            request.getHeaders().add("X-Client-Port", serverPort);
+            log.info("URL {} için X-Client-Port eklendi: {}", request.getURI(), serverPort);
+        } else {
+            log.warn("URL {} için port bilgisi bulunamadı!", request.getURI());
+        }
+
         return execution.execute(request, body);
     }
 }
-
-
