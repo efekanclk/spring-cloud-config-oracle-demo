e6ed2c657957a17c8b19247aaf5887f6ed687627
efekancl <efecelik4207@gmail.com>
2026-08-01 18:13:39 +0300

    ClientApplication'daki değişken isimleri güncellendi.

--- client/src/main/java/com/efekan/client/ClientApplication.java
+++ client/src/main/java/com/efekan/client/ClientApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.bootstrap.BootstrapRegistry;
 import org.springframework.boot.bootstrap.BootstrapRegistryInitializer;
 import org.springframework.boot.env.YamlPropertySourceLoader;
-import org.springframework.cloud.config.client.ConfigClientRequestTemplateFactory;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.config.client.ConfigClientRequestTemplateFactory;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,35 +18,36 @@ import java.util.List;
 public class ClientApplication {
 
     public static void main(String[] args) {
-        var efe = new SpringApplication(ClientApplication.class);
-        efe.addBootstrapRegistryInitializer(new BootstrapRegistryInitializer() {
+        var springApplication = new SpringApplication(ClientApplication.class);
+
+        springApplication.addBootstrapRegistryInitializer(new BootstrapRegistryInitializer() {
             @Override
             public void initialize(BootstrapRegistry registry) {
                 try {
-                    String port = readPort();
+                    String serverPort = readPort();
                     registry.registerIfAbsent(RestTemplate.class, context -> {
-                        RestTemplate degisken = context.get(ConfigClientRequestTemplateFactory.class).create();
-                        degisken.getInterceptors().add(new CustomInterceptor(port));
-                        return degisken;
+                        RestTemplate restTemplate = context.get(ConfigClientRequestTemplateFactory.class).create();
+                        restTemplate.getInterceptors().add(new CustomInterceptor(serverPort));
+                        return restTemplate;
                     });
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    throw new RuntimeException("Port okunurken bir hata oluştu", e);
                 }
             }
 
             public String readPort() throws IOException {
-                List<PropertySource<?>> abc = new YamlPropertySourceLoader().load("İsim", new ClassPathResource("application.yml"));
-                StandardEnvironment anil = new StandardEnvironment();
-                abc.forEach(source -> anil.getPropertySources().addLast(source));
-                String port = anil.getProperty("server.port");
+                List<PropertySource<?>> propertySources = new YamlPropertySourceLoader().load("applicationYaml", new ClassPathResource("application.yml"));
+                StandardEnvironment environment = new StandardEnvironment();
+                propertySources.forEach(source -> environment.getPropertySources().addLast(source));
+                String port = environment.getProperty("server.port");
 
                 if (port == null || port.isEmpty()) {
-                    throw new RuntimeException();
+                    throw new RuntimeException("server.port değeri bulunamadı!");
                 }
                 return port;
             }
         });
-        efe.run(args);
-    }
 
+        springApplication.run(args);
+    }
 }
--- server/src/main/java/com/efekan/server/controller/ConfigEnvironmentController.java
+++ server/src/main/java/com/efekan/server/controller/ConfigEnvironmentController.java
@@ -1,30 +1,19 @@
-//bu sınıf dışarıdan gelen istekleri karşılayan ve yanıt döndüren bir kapıdır
-
 package com.efekan.server.controller;
 
 import com.efekan.server.environment.ConfigPropertyRequest;
 import com.efekan.server.service.CEStore;
 import com.efekan.server.service.ConfigEnvironmentService;
-//üstteki iki  satır ile servis ve dto sınıfını bu sınıfa tanıtıyoruz.
 
 import com.efekan.server.model.ConnectedUsers;
-//connectedusers sınıfını bu sınıfa tanıtıyoruz.
-import org.springframework.http.ResponseEntity;
-//dış dünyaya dönen http yanıtını kapsüllemek için kullanılan spring sınıfını proje dahil etmek için kullanılıyor.
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-//yukarıdakiler ise spring web anotasyonlarını projeye dahil etmek için kullanılıyor.
-import jakarta.servlet.http.HttpServletRequest;
-//spring'in istemciden gelen http isteğinin detaylarına erişmemizi sağlar.
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-//java'nın harita ve çoklu iş parçacığı güvenli veri yapılarını projeye dahil eder
 
-@RestController //bu sınıfın bir restcontroller olduğunu bildirir. verilerin json veya metin olarak body'ye yazılmasını sağlar.
-@RequestMapping("/api/v1/config") //controler için ana endpoint yolunu yani base URL'i tanımlamamızı sağlar.
+@RestController
+@RequestMapping("/api/v1/config")
 
 public class ConfigEnvironmentController {
 
@@ -38,19 +27,19 @@ public class ConfigEnvironmentController {
 
     // https://gelecegiyazanlar.turkcell.com.tr/egitimler/java
 
-    // Db'dekı tüm property'lerı lısteleyen apı: Gerıye lıste dönderecek,dışarıdan applıcatıon veya profıle veya label veya key alabılır.
-    // Bu gelenlere göre db'den verılerı fıltreleyerek ve pagınatıon yaparak çeker.
-    // Eğer yoksa tüm property'lerı pagınatıon ıle çeker. Bunlar return edılıır. 201 durum kodu döner
+    // Db'deki tüm property'leri lısteleyen API: Geriye liste dönderecek,dışarıdan application veya profile veya label veya key alabilir.
+    // Bu gelenlere göre DB'den verileri filtreleyerek ve pagination yaparak çeker.
+    // Eğer yoksa tüm property'leri pagination ile çeker. Bunlar return edilir. 201 durum kodu döner
 
 
-    // Db'ye yenı bır porpery ekleyen apı: Gerıye eklenen property'ı dönderır.
-    // dışarıdan applıcatıon ve profıle ve label ve key ve value alır. Bunların herhangı bırı null veya boş ıse hata fırlatır.
-    // not:eklenen property ıçın ılgılı clıent refresh apsı çağırılıcak (şımdı yapılmayacak)
+    // Db'ye yenir bir property ekleyen apı: Geriye eklenen property'i dönderir.
+    // dışarıdan application ve profile ve label ve key ve value alır. Bunların herhangı biri null veya boş ise hata fırlatır.
+    // not:eklenen property için ilgili client refresh API'si çağrılacak. (Şimdi yapılmayacak.)
 
     @PutMapping("/property")
     public String updateProperty(@RequestBody ConfigPropertyRequest request) {
-        // dışarıdan applıcatıon ve profıle ve label ve key ve value alır. Bunların herhangı bırı null veya boş ıse hata fırlatır.
-        // update edıldıkten sonra yenı property return edılıır
+        // Db'deki tüm property'leri lısteleyen API: Geriye liste dönderecek,dışarıdan application veya profile veya label veya key alabilir.
+        // Update edildikten sonra yeni property return edilir.
         boolean isUpdated = configEnvironmentService.updateConfig(request);
         if (isUpdated) {
             //not: ceStore'dan application ve profile'e göre veri bulunacak. Bulunan verinin ipAddress'ine /actuator/refresh apisine post isteği at. Şimdi yapılmayacak.
@@ -59,8 +48,8 @@ public class ConfigEnvironmentController {
         throw new RuntimeException("bulunamadı");
     }
 
-    // Db'dekı property'ı sılen apı; Bu apı dışarıdan applıcatıon ve profıle ve key alır.
-    // Bulunan kayıt db'den sılınır ve gerıye 204 durum kodu döner ve hıçbışey return etmez
+    // Db'deki property'i silen API; Bu API dışarıdan application ve profile ve key alır.
+    // Bulunan kayıt db'den silinir ve gerıye 204 durum kodu döner ve hiçbir şey return etmez
 
     @GetMapping("/connected-users/")
     public Map<String, ConnectedUsers> getConnectedUser() {
