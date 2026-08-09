883543dfc0d0fa1518986f4724b760b427ad94b1
efekancl <efecelik4207@gmail.com>
2026-08-09 16:40:39 +0300

    Aynı config profiline bağlanan birden fazla istemciyi takip edebilmek için CEStore altyapısı güncellendi

--- server/src/main/java/com/efekan/server/service/CEStore.java
+++ server/src/main/java/com/efekan/server/service/CEStore.java
@@ -1,21 +1,29 @@
 package com.efekan.server.service;
 
 import com.efekan.server.model.ConnectedUsers;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class CEStore {
-    private final Map<String, ConnectedUsers> connectedUsersMap = new ConcurrentHashMap<>();
 
-    public Map<String , ConnectedUsers> getConnectedUsersMap() {return connectedUsersMap;}
+    private final Map<String, List<ConnectedUsers>> connectedUsersMap = new ConcurrentHashMap<>();
 
-    public void put(String application, String profile, String ipAddress){
+    public Map<String, List<ConnectedUsers>> getConnectedUsersMap() {
+        return connectedUsersMap;
+    }
+
+    public void put(String application, String profile, String ipAddress) {
         String uniqueKey = application + "-" + profile;
         ConnectedUsers connectedUser = new ConnectedUsers(application, profile, ipAddress);
-        connectedUsersMap.put(uniqueKey, connectedUser);
+
+        List<ConnectedUsers> connectedUsers = connectedUsersMap.computeIfAbsent(uniqueKey, k -> new ArrayList<>());
+        if (!connectedUsers.contains(connectedUser)) {
+            connectedUsers.add(connectedUser);
+        }
     }
 }
