0c0cd761d9d88903c5622df880b0600015225a81
efekancl <efecelik4207@gmail.com>
2026-08-11 21:10:57 +0300

    thymleaf arayüzüne insert formu ve index sayfasına insert butonu eklendi

--- server/src/main/java/com/efekan/server/controller/ConfigWebController.java
+++ server/src/main/java/com/efekan/server/controller/ConfigWebController.java
@@ -1,5 +1,6 @@
 package com.efekan.server.controller;
 
+import ch.qos.logback.core.model.processor.PhaseIndicator;
 import com.efekan.server.db.entity.ConfigProperty;
 import com.efekan.server.db.repository.ConfigPropertyPropertyRepository;
 import com.efekan.server.model.ConnectedUsers;
@@ -52,6 +53,19 @@ public class ConfigWebController {
     @PostMapping("/delete/{id}")
     public String deleteProperty(@PathVariable String id) {
         repository.findById(id).ifPresent(repository::delete);
+        return "redirect:/web/config";
+    }
+
+    @GetMapping("/insert")
+    public String insertForm(Model model){
+        model.addAttribute("property", new ConfigProperty());
+        return "insert";
+    }
+
+    @PostMapping("/save")
+    public String insertProperty(@ModelAttribute ConfigProperty property){
+        repository.save(property);
+        ensembleProperty(property.getApplication(), property.getProfile());
         return "redirect:/web/config";
     }
 
