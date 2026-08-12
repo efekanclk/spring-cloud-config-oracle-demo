339ea5296577e4fb9a75336792ebb908b5c76e52
efekancl <efecelik4207@gmail.com>
2026-08-12 14:15:12 +0300

    asenkron metod oluşturmak için gereken EnableAsync etiketi eklendi

--- server/src/main/java/com/efekan/server/ServerApplication.java
+++ server/src/main/java/com/efekan/server/ServerApplication.java
@@ -3,9 +3,11 @@ package com.efekan.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAsync
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
---
+++ server/src/main/java/com/efekan/server/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.efekan.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncExecutor {
+
+    @Bean(name = "asyncExecutor")
+    public Executor syncExecutor()
+    {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(3);
+//        executor.setMaxPoolSize(3);
+//        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsenkronThread - ");
+        executor.initialize();
+        return executor;
+    }
+}
