d44c72ea8774992364329e060ebff4c81927cafd
efekancl <efecelik4207@gmail.com>
2026-08-13 22:32:53 +0300

    Client uygulamasından Server'a ping atmak için ClientPingService class'ı oluşturuldu ve ClientApplication'a @EnableScheduling eklendi

--- client/src/main/java/com/efekan/client/ClientApplication.java
+++ client/src/main/java/com/efekan/client/ClientApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.config.client.ConfigClientRequestTemplateFactory;
 
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClientApplication {
 
     public static void main(String[] args) {
---
+++ client/src/main/java/com/efekan/client/ClientPingService.java
@@ -0,0 +1,44 @@
+package com.efekan.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class ClientPingService {
+    private static final Logger log = LoggerFactory.getLogger(ClientPingService.class);
+    private final RestClient restClient = RestClient.builder().build();
+
+    @Value("${spring.application.name}")
+    private String application;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Value("${spring.config.import}")
+    private String url;
+
+
+    @Scheduled(initialDelay = 5000,fixedDelay = 5000)
+    public String sendPing() {
+        try {
+            String baseUrl = url.replace("configserver:", "");
+            String targetUrl = baseUrl + "/" + application + "/" + profile;
+
+            restClient.get()
+                    .uri(targetUrl)
+                    .retrieve()
+                    .toBodilessEntity();
+
+            log.info("ping atıldı: {} - {}", application, profile);
+        } catch (Exception e) {
+            String a = e.getLocalizedMessage();
+            log.info(a);
+            System.out.println("Refresh başarısız");
+        }
+    return "efekan";}
+}
+
