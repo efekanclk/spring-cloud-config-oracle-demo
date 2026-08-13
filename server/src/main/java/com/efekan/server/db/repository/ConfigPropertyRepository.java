31e24f050a87dc496380878ab9f7333797f16b34
efekancl <efecelik4207@gmail.com>
2026-08-13 12:48:14 +0300

    dosya ismi değiştirildi

--- server/src/main/java/com/efekan/server/db/repository/ConfigPropertyRepository.java
+++ server/src/main/java/com/efekan/server/db/repository/ConfigPropertyRepository.java
@@ -3,7 +3,7 @@ package com.efekan.server.db.repository;
 import com.efekan.server.db.entity.ConfigProperty;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ConfigPropertyPropertyRepository extends CrudRepository<ConfigProperty, String> {
+public interface ConfigPropertyRepository extends CrudRepository<ConfigProperty, String> {
 
     ConfigProperty findByApplicationAndProfileAndLabelAndKey(String application, String profile, String label, String key);
 
